72de8d3c99a8ca655ff595ba503e114ca5199146
Iurochkin Sergei <41903077+SilverRid@users.noreply.github.com>
2022-07-08 11:15:51 +0300

    Inititial project

---
+++ src/main/java/com/ikg/integrationwithkudago/IntegrationWithKudaGoApplication.java
@@ -0,0 +1,13 @@
+package com.ikg.integrationwithkudago;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntegrationWithKudaGoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IntegrationWithKudaGoApplication.class, args);
+    }
+
+}
